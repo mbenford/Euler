8b1affd743097066a1794c29c3f1068ccf9769c2
Michael Benford <michael@michaelbenford.net>
2011-07-22 08:42:04 -0300

    Problema 3 resolvido, mas algoritmo de fatoracao pode ser melhorado

---
+++ Problems/Problem3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler.Tools;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem3 : Problem
+    {
+        public override Solution Solve()
+        {                                  
+            long value = Toolbox.Factorize(600851475143)
+                .Where(x => Toolbox.IsPrime(x))
+                .Max();
+
+            return new Solution(value);
+        }
+    }
+}
--- Tools/Toolbox.cs
+++ Tools/Toolbox.cs
@@ -19,6 +19,54 @@ namespace ProjectEuler.Tools
                 y = sum;
                 yield return y;
             }
-        }        
+        }
+        
+        public static IEnumerable<long> Factorize(long number)
+        {
+            var factors = new List<long>();
+
+            if (Toolbox.IsPrime(number))
+                factors.Add(number);
+            else
+            {
+                long divisor = 2;
+                long quotient = number;
+                while (quotient > 1)
+                {
+                    if (quotient % divisor == 0)
+                    {
+                        factors.Add(divisor);
+                        quotient /= divisor;
+                    }
+                    else
+                        divisor++;
+                }
+            }
+
+            return factors;
+        }
+
+        public static bool IsPrime(long number)
+        {
+            bool isPrime;
+
+            if (number > 2 && number % 2 == 0)
+                isPrime = false;
+            else
+            {
+                isPrime = true;
+                int max = (int)Math.Truncate(Math.Sqrt(number));
+                for (int i = 2; i <= max; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            return isPrime;
+        }
     }
 }
