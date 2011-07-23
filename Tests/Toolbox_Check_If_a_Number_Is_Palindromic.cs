c6e6d854c2da460f5fa85f475d71640bbd98575a
Michael Benford <michael@michaelbenford.net>
2011-07-23 00:41:42 -0300

    Acertos nos nomes dos metodos de teste de numeros primos e da sequencia de Fibonacci

---
+++ Tests/Toolbox_Check_If_a_Number_Is_Palindromic.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Toolbox_Check_If_a_Number_Is_Palindromic
+    {
+        [Test]
+        public void Returns_True_When_Passing_1()
+        {
+        }
+    }
+}
--- Tests/Toolbox_Check_If_a_Number_Is_Prime.cs
+++ Tests/Toolbox_Check_If_a_Number_Is_Prime.cs
@@ -11,67 +11,67 @@ namespace ProjectEuler.Tests
     public class Toolbox_Check_If_a_Number_Is_Prime
     {
         [Test]
-        public void Return_True_When_Passing_2()
+        public void Returns_True_When_Passing_2()
         {
             Assert.IsTrue(Toolbox.IsPrime(2));
         }
 
         [Test]
-        public void Return_True_When_Passing_3()
+        public void Returns_True_When_Passing_3()
         {
             Assert.IsTrue(Toolbox.IsPrime(3));
         }
 
         [Test]
-        public void Return_False_When_Passing_4()
+        public void Returns_False_When_Passing_4()
         {
             Assert.IsFalse(Toolbox.IsPrime(4));
         }
 
         [Test]
-        public void Return_True_When_Passing_5()
+        public void Returns_True_When_Passing_5()
         {
             Assert.IsTrue(Toolbox.IsPrime(5));
         }
 
         [Test]
-        public void Return_False_When_Passing_6()
+        public void Returns_False_When_Passing_6()
         {
             Assert.IsFalse(Toolbox.IsPrime(6));
         }
 
         [Test]
-        public void Return_True_When_Passing_7()
+        public void Returns_True_When_Passing_7()
         {
             Assert.IsTrue(Toolbox.IsPrime(7));
         }
 
         [Test]
-        public void Return_False_When_Passing_8()
+        public void Returns_False_When_Passing_8()
         {
             Assert.IsFalse(Toolbox.IsPrime(8));
         }
 
         [Test]
-        public void Return_False_When_Passing_9()
+        public void Returns_False_When_Passing_9()
         {
             Assert.IsFalse(Toolbox.IsPrime(9));
         }
 
         [Test]
-        public void Return_True_When_Passing_29()
+        public void Returns_True_When_Passing_29()
         {
             Assert.IsTrue(Toolbox.IsPrime(29));
         }
 
         [Test]
-        public void Return_True_When_Passing_13()
+        public void Returns_True_When_Passing_13()
         {
             Assert.IsTrue(Toolbox.IsPrime(13));
         }
 
         [Test]
-        public void Return_True_When_Passing_19()
+        public void Returns_True_When_Passing_19()
         {
             Assert.IsTrue(Toolbox.IsPrime(19));
         }
