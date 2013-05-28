87bb912ab023d35bdec45001e7e7d0a254580d42
Michael Benford <michael@michaelbenford.net>
2013-05-28 01:24:06 -0300

    Problema 19 resolvido

--- Tests/Problem_Solutions_Remain_Correct.cs
+++ Tests/Problem_Solutions_Remain_Correct.cs
@@ -1,123 +1,42 @@
 ﻿using System.Numerics;
 using Euler.Problems;
+using FluentAssertions;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Euler.Tests
 {
     public class Problem_Solutions_Remain_Correct
     {
-        [Fact]
-        public void Solution_For_Problem_1_Remains_Correct()
+        [Theory]
+        [InlineData(1, 233168)]
+        [InlineData(2, 4613732)]
+        [InlineData(3, 6857)]
+        [InlineData(4, 906609)]
+        [InlineData(5, 232792560)]
+        [InlineData(6, 25164150)]
+        [InlineData(7, 104743)]
+        [InlineData(8, 40824)]
+        [InlineData(9, 31875000)]
+        [InlineData(10, 142913828922)]
+        [InlineData(11, 70600674)]
+        [InlineData(12, 76576500)]
+        [InlineData(13, 5537376230)]
+        [InlineData(14, 837799)]
+        [InlineData(15, 137846528820)]
+        [InlineData(16, 1366)]
+        [InlineData(17, 21124)]
+        [InlineData(19, 171)]        
+        public void Checks_If_Solution_Remains_Correct(int problemId, long expectedValue)
         {
-            Check(1, 233168);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_2_Remains_Correct()
-        {
-            Check(2, 4613732);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_3_Remains_Correct()
-        {
-            Check(3, 6857);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_4_Remains_Correct()
-        {
-            Check(4, 906609);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_5_Remains_Correct()
-        {
-            Check(5, 232792560);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_6_Remains_Correct()
-        {
-            Check(6, 25164150);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_7_Remains_Correct()
-        {
-            Check(7, 104743);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_8_Remains_Correct()
-        {
-            Check(8, 40824);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_9_Remains_Correct()
-        {
-            Check(9, 31875000);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_10_Remains_Correct()
-        {
-            Check(10, 142913828922);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_11_Remains_Correct()
-        {
-            Check(11, 70600674);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_12_Remains_Correct()
-        {
-            Check(12, 76576500);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_13_Remains_Correct()
-        {
-            Check(13, 5537376230);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_14_Remains_Correct()
-        {
-            Check(14, 837799);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_15_Remains_Correct()
-        {
-            Check(15, 137846528820);
-        }
-
-        [Fact]
-        public void Solution_For_Problem_16_Remains_Correct()
-        {
-            Check(16, 1366);
-        }
-        
-        [Fact]
-        public void Solution_For_Problem_17_Remains_Correct()
-        {
-            Check(17, 21124);
-        }
-
-        private void Check(int problemId, BigInteger expectedValue)
-        {
-            Solution solution = GetSolution(problemId);
-            Assert.True(expectedValue == solution.Value);
-        }
-
-        private Solution GetSolution(int problemId)
-        {
+            // Arrange
             Problem problem = ProblemFactory.GetProblem(problemId);
-            return problem.Solve();
+
+            // Act
+            Solution solution = problem.Solve();
+
+            // Assert
+            solution.Value.Should().Be(new BigInteger(expectedValue));
         }
     }
 }
